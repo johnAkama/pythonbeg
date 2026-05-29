b1ea19b45b8a47ee15e50553073d0aa4aca55043
Kodjo Akama <johnakama32@gmail.com>
2026-05-29 20:01:43 +0000

    progress updat

---
+++ rpg_character.py
@@ -0,0 +1,31 @@
+print("hello".__len__())full_dot = '●'
+empty_dot = '○'
+
+def create_character (name,strength,intelligence,charisma):
+    if not isinstance(name, str) or isinstance(name, int):
+        return "The character name should be a string"
+    
+    if name.__len__() == 0:
+        return "The character should have a name"
+    if (name.__len__()) > 10:
+        return "The character name is too long"
+
+    if ' ' in name:
+        return "The character name should not contain spaces"
+    
+    if not isinstance(strength,int) or not isinstance(intelligence,int) or not isinstance(strength,int) or not isinstance(charisma,int):
+        return "All stats should be integers"
+
+    if strength <1 or intelligence < 1 or charisma < 1:
+        return "All stats should be no less than 1"
+
+    if strength > 4 or intelligence > 4 or charisma > 4:
+        return "All stats should be no less than 1"
+
+
+    
+    
+
+test = create_character ('john',4,0,4)
+
+print(test)
