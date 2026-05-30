1befff4abb3d2d77134068cb100eee1c216e8bc4
Kodjo Akama <johnakama32@gmail.com>
2026-05-30 11:41:03 +0000

    update progress

--- rpg_character.py
+++ rpg_character.py
@@ -1,4 +1,4 @@
-print("hello".__len__())full_dot = '●'
+full_dot = '●'
 empty_dot = '○'
 
 def create_character (name,strength,intelligence,charisma):
@@ -20,7 +20,10 @@ def create_character (name,strength,intelligence,charisma):
         return "All stats should be no less than 1"
 
     if strength > 4 or intelligence > 4 or charisma > 4:
-        return "All stats should be no less than 1"
+        return "All stats should be no more than 4"
+
+    if (strength+intelligence+charisma) != 7:
+        return "The character should start with 7 points"
 
 
     
