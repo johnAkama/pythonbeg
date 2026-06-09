026dd8940ec9cb13fe4909f19feb1e29e5125133
Kodjo Akama <johnakama32@gmail.com>
2026-06-09 14:17:49 +0000

    progress update

--- zip_enum.py
+++ zip_enum.py
@@ -20,4 +20,22 @@ for index, country in list_with_enum:
     if index >= 5:
         break
     else:
-        print(f"first batch of coountry\n{index}: {country}")
+        #print(f"first batch of coountry\n{index}: {country}")
+        continue;
+
+
+developers = ['Naomi', 'Dario', 'Jessica', 'Tom']
+ids = [1, 2, 3, 4]
+
+
+new_list = list(zip(developers,ids))
+#print(new_list)
+
+
+even_num = []
+
+for num in range(21):
+    if num%2 == 0:
+       even_num.push(num)
+    else:
+        print(f'{num} is not an even number \n')
